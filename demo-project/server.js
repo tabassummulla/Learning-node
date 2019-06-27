deb9dd8e17425534d3a68b036ae59c8f2fe8d274
Tabassum <tabassum.mulla@academytrainee.com>
2019-06-27 23:21:55 +0100

    changed port number

--- demo-project/server.js
+++ demo-project/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const path = require('path');
-const port = 1000;
+const port = 3000;
 
 app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: true }));
