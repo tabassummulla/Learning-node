0be86975ed59ee3dd93f539cb3180c1748029b2c
tabassummulla <tabassum.mulla7@yahoo.co.uk>
2019-06-27 17:47:48 +0100

    fromatted files

--- demo-project/db.js
+++ demo-project/db.js
@@ -1,19 +1,19 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize('develop','root', 'password', {
+const db = new Sequelize('develop', 'root', 'password', {
 
-          host: 'localhost',
-          dialect : 'mysql', 
-          define: {
-               charset :'utf8mb4',
-               collate: 'utf8mb4_unicode_ci',
-               timestamps: false,
-               freezeTableName: true 
-          },
-          pool : {
+     host: 'localhost',
+     dialect: 'mysql',
+     define: {
+          charset: 'utf8mb4',
+          collate: 'utf8mb4_unicode_ci',
+          timestamps: false,
+          freezeTableName: true
+     },
+     pool: {
           min: 0,
-          max : 5
-          }
+          max: 5
+     }
 
 
 });
@@ -21,33 +21,34 @@ const db = new Sequelize('develop','root', 'password', {
 
 const User = db.define('users', {
 
-          first_name: {
-                   type: Sequelize.STRING,
-                   allowNull:false
-          },
-          last_name: {
-                    type: Sequelize.STRING,
-                    allowNull:false
-           },
-           password: {
-                    type: Sequelize.STRING,
-                    
-                    allowNull:false,
-                          
-           },
-           email_add: {
-                    type: Sequelize.STRING(128),
-                    allowNull:false,
-                    primaryKey:true
-           },
-      // Timestamps
-      createdAt: {
+     first_name: {
+          type: Sequelize.STRING,
+          allowNull: false
+     },
+     last_name: {
+          type: Sequelize.STRING,
+          allowNull: false
+     },
+     password: {
+
+          //TODO: use bcrypt to encrypt data 
+          type: Sequelize.STRING(20),
+          allowNull: false,
+
+     },
+     email_add: {
+          type: Sequelize.STRING(128),
+          allowNull: false,
+          primaryKey: true
+     },
+     // Timestamps
+     createdAt: {
           type: Sequelize.DATE,
           defaultValue: Sequelize.fn('NOW')
      },
-      updatedAt: {
-           type: Sequelize.DATE,
-           // DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP - how it works on my sql TODO: find how it works here 
+     updatedAt: {
+          type: Sequelize.DATE,
+          // DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP - how it works on my sql TODO: find how it works here 
           default: Sequelize.fn('NOW')
      }
 
@@ -58,8 +59,8 @@ const User = db.define('users', {
 
 
 db.sync().then(
-          ()=> console.log("*****Database created and synced")
-          ).catch((err) => console.log(err));
+     () => console.log("*****Database created and synced")
+).catch((err) => console.log(err));
 
 exports = module.exports = {
      User
--- demo-project/server.js
+++ demo-project/server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const port = 1000;
 
 app.use(bodyParser.json())
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use('/', express.static(path.join(__dirname, '/html')));
 app.use('/api', require('./routes/api').route);
 
 
 
 
-app.listen(port, ()=> console.log('**Server started on port', port));
+app.listen(port, () => console.log('**Server started on port', port));
