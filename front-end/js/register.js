ab29f1a766f03b6c8e9688993135285f1b35cb53
tabassummulla <tabassum.mulla7@yahoo.co.uk>
2019-07-09 17:02:03 +0100

    validation for registration complete, and attempted to remove redundancy

--- front-end/js/register.js
+++ front-end/js/register.js
@@ -1,49 +1,57 @@
 
-$(document).ready(function(){
+
+$(document).ready(function () {
+
+  var timer = null;
+  $('#inputEmail-Reg').keydown(function () {
+    clearTimeout(timer);
+    timer = setTimeout(validateEmail, 1000);
+
+  });
+
+  $('html').keyup(function(e){
+    if(e.keyCode == 8 ) {
+
+      let errAlert = document.getElementById('errRegMsg');
+      let signUpErr = document.getElementById('signUpErr');
+
+      errAlert.visibility ="hidden";
+      signUpErr.innerHTML = "";
+    }
+});
+
+  $('#inputPassword-RegConfirm').keydown(function () {
+    clearTimeout(timer);
+    timer = setTimeout(checkPasswordMatch, 1000);
+
+  });
 
 
-let timer = null;
-$('#inputEmail-Reg').keydown(function(){
-          clearTimeout(timer); 
-          timer = setTimeout(validateEmail, 1000);
+
 
 });
 
 
-$('#inputPassword-RegConfirm').keydown(function(){
-          clearTimeout(timer); 
-          timer = setTimeout(checkPasswordMatch, 1000);
+function checkPasswordMatch() {
+  let password = document.getElementById('inputPassword-Reg');
+  let confirmPass = document.getElementById('inputPassword-RegConfirm');
+  let errAlert = document.getElementById('errRegMsg');
+  let signUpErr = document.getElementById('signUpErr');
 
-});
+  if (password.value !== confirmPass.value) {
 
+    errAlert.style.visibility = "visible";
+    errAlert.className = "alert alert-danger";
+    signUpErr.style.color = "red";
 
-
-
-});
-
-
-function checkPasswordMatch(){
- let password = document.getElementById('inputPassword-Reg').value;
-let confirmPass = document.getElementById('inputPassword-RegConfirm').value;
-let errAlert = document.getElementById('errRegMsg');
-let signUpErr = document.getElementById('signUpErr');
-
-if(!(password === "" && confirmPass ==="")) {
-
-if(password !== confirmPass) {
-          errAlert.style.visibility = "visible";
-          document.getElementById('errRegMsg').className="alert alert-danger";
-          document.getElementById('signUpErr').style.color = "red";
-          signUpErr.innerHTML = "Passwords do not match!";
-}
-else{
-         
-          errAlert.style.visibility = "hidden";
-          signUpErr.innerHTML = "";
-}
-
-
-}
+    password.focus();
+    confirmPass.focus();
+    signUpErr.innerHTML = "Passwords do not match!";
+  }
+  else {
+    errAlert.style.visibility = "hidden";
+    signUpErr.innerHTML = "";
+  }
 
 }
 
@@ -52,115 +60,162 @@ else{
 
 
 
-function validateEmail(){
-let email = document.getElementById('inputEmail-Reg').value;
-let errAlert = document.getElementById('errRegMsg');
-let signUpErr = document.getElementById('signUpErr');
-          
-let emailFormat = /\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6}/;
+function validateEmail() {
+  let email = document.getElementById('inputEmail-Reg');
+  let errAlert = document.getElementById('errRegMsg');
+  let signUpErr = document.getElementById('signUpErr');
 
-document.getElementById('errRegMsg').className="alert alert-danger";
-document.getElementById('signUpErr').style.color = "red";
+  let emailFormat = /\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6}/;
 
-                    if(!(email.match(emailFormat))){
-                              
+  errAlert.className = "alert alert-danger";
+  signUpErr.style.color = "red";
 
-                    errAlert.style.visibility = "visible";
-                    signUpErr.innerHTML = "Please enter a valid email address";
-                    
-                    }
-                    else{
+  if(email.value !== ""){
 
+  if (!(email.value.match(emailFormat))) {
 
-                              emailExists(email);
-                    }
-                    
-                    
+    errAlert.style.visibility = "visible";
+    signUpErr.innerHTML = "Please enter a valid email address";
+    email.focus();
+
+  }
+  else {
+    emailExists(email.value);
+
+  }
+}else{
+  errAlert.style.visibility = "hidden";
+
+}
 }
 
 
-function validateRegForm(){
+function validateRegForm() {
 
-  
-let email = document.getElementById('inputEmail-Reg').value;
-let password = document.getElementById('inputPassword-Reg').value;
-let confirmPass = document.getElementById('inputPassword-RegConfirm').value;
-let firstName = document.getElementById('inputFName-Reg').value;
-let lastName = document.getElementById('inputLastName-Reg').value;
+  let email = document.getElementById('inputEmail-Reg');
+  let password = document.getElementById('inputPassword-Reg');
+  let confirmPass = document.getElementById('inputPassword-RegConfirm');
+  let firstName = document.getElementById('inputFName-Reg');
+  let lastName = document.getElementById('inputLastName-Reg');
+  let passwordFormat = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\w{6,}$/;
 
-let errAlert = document.getElementById('errRegMsg');
-let signUpErr = document.getElementById('signUpErr');
+  //Alert message 
+  let errAlert = document.getElementById('errRegMsg');
+  let signUpErr = document.getElementById('signUpErr');
 
-let passwordFormat =  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\w{6,}$/;
+  signUpErr.innerHTML = "";
+  errAlert.className = "alert alert-danger";
+  signUpErr.style.color = "red";
 
-errAlert.style.visibility = "visible";
-signUpErr.innerHTML ="";   
-document.getElementById('errRegMsg').className="alert alert-danger";
-document.getElementById('signUpErr').style.color = "red";
+  if (email.value === "" || password.value === "" || confirmPass.value === "" || firstName.value === "" || lastName.value === "") {
 
-if(email === "" || password === "" || confirmPass === "" || firstName ==="" || lastName === "" ){
+    errAlert.style.visibility = "visible";
+    signUpErr.innerHTML = "Please fill in all required (*) fields";
+    return false;
+  }
+  if (password.value !== confirmPass.value) {
 
-signUpErr.innerHTML = "Please fill in all required fields";
+    errAlert.style.visibility = "visible";
+    signUpErr.innerHTML = "Passwords do not match!";
+    return false;
 
-}else if(password !== "") {
+  } else if (!(password.value.match(passwordFormat))) {
+    errAlert.style.visibility = "visible";
+    signUpErr.innerHTML = "Password must contain:" + "\n" + "at least one digit/lowercase/uppercase letter and be at least six characters long";
+    return false;
+  }
 
-if(!(password.match(passwordFormat))){
-
-signUpErr.innerHTML = "Password must contain:" +"\n" +"at least one digit/lowercase/uppercase letter and be at least six characters long";
-
-}
-
-}else if(!(password === "" && confirmPass=== "")){
-
-          if(password === confirmPass) {
-          signUpErr.innerHTML = "Passwords do not match!";
-
-          }
-          
+  signUp(email.value, firstName.value, lastName.value, password.value);
 
 
 }
 
+
+
+
+
+
+
+
+function emailExists(email) {
+
+  let errAlert = document.getElementById('errRegMsg');
+  let signUpErr = document.getElementById('signUpErr');
+  let request = new XMLHttpRequest();
+  let url = '/api/register/exists';
+
+  request.open('POST', url);
+  const body = {
+    email_add: email,
+
+  };
+  request.setRequestHeader("Content-Type", "application/json");
+
+  request.onload = function () {
+
+    errAlert.style.visibility = "visible";
+
+    if (request.status === 409) {
+
+      signUpErr.innerHTML = "Email is already registered  " + "<a href='http://localhost:3000/index.html'> Login Now </a>";
+
+    }
+    else if (request.status === 200) {
+
+      errAlert.className = "alert alert-success";
+      signUpErr.style.color = "green";
+      signUpErr.innerHTML = request.response;
+
+    }
+
+
+  };
+
+  request.send(JSON.stringify(body));
+
 }
 
 
+function signUp(email, firstName, lastName, password) {
 
-function emailExists(email){
+  let errAlert = document.getElementById('errRegMsg');
+  let signUpErr = document.getElementById('signUpErr');
+
+  let request = new XMLHttpRequest();
+  let url = "/api/register";
+
+  request.open('POST', url);
+
+  const body = {
+    email_add: email,
+    first_name: firstName,
+    last_name: lastName,
+    password: password
+
+  };
+  request.setRequestHeader("Content-Type", "application/json");
+
+  request.onload = function () {
+
+    errAlert.style.visibility = "visible";
+
+    if (request.status === 200) {
+
+      errAlert.className = "alert alert-success";
+      signUpErr.style.color = "green";
+      signUpErr.innerHTML = request.response;
+
+    }
+    else {
+
+      signUpErr.innerHTML = request.response;
+    }
 
 
-let errAlert = document.getElementById('errRegMsg');
-let signUpErr = document.getElementById('signUpErr');
 
+  };
 
-let request = new XMLHttpRequest();
-let url = '/api/register/exists';
-         
-request.open('POST', url);
-const body = {
-          email_add: email,
-
-};
-request.setRequestHeader("Content-Type", "application/json");
-
-request.onload= function() {
-
-errAlert.style.visibility = "visible";
-
-if(request.status === 409){
-
-signUpErr.innerHTML = "Email is already registered  "+ "<a href='http://localhost:3000/index.html'> Login Now </a>";
-
-}
-else if(request.status === 200){
-
-          document.getElementById('errRegMsg').className="alert alert-success";
-          document.getElementById('signUpErr').style.color = "green";
-          signUpErr.innerHTML = request.response;
-          
-          }
-};
-
-request.send(JSON.stringify(body));
+  request.send(JSON.stringify(body));
 
 
 
