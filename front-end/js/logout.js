a116a7a3b6f3920bee85e6715aff9b9c615adf79
tabassummulla <tabassum.mulla7@yahoo.co.uk>
2019-07-16 16:32:28 +0100

    redirect on logout success to login page

--- front-end/js/logout.js
+++ front-end/js/logout.js
@@ -2,22 +2,16 @@
 function logout() {
     let request = new XMLHttpRequest();
     let url = '/api/logout';
-   
     request.open('GET', url);
-
-    // request.setRequestHeader("Content-Type", "application/json");
-    request.onload = function () {
-
-        if(request.response.status === 200){
-            
-        window.location.href='http://localhost:3000/login';
-        }
-              
-    };
-
-
     request.send();
 
+    let delayInMilliseconds = 1000; //1 second
+
+    setTimeout(function() {
+
+        window.location.href= '/login';
+        
+    }, delayInMilliseconds);
 
 }
 
