2c8520c311c15fc032cb6437b5c4624657a88946
tabassummulla <tabassum.mulla7@yahoo.co.uk>
2019-07-16 16:34:38 +0100

    not sure if i need this? - similar to profile

---
+++ routes/api/myaccount.js
@@ -0,0 +1,62 @@
+
+const User = require('../../database/db').User;
+const route = require('express').Router();
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
+
+route.get('/', (req, res) => {
+
+    if (!req.session.loggedin) {
+        res.redirect(401, '/login');
+    }
+
+    User.findOne({
+        where: {
+            email_add: req.session.username
+        }
+    }).then((users) => {
+
+        res.status(200).json(users);
+
+    }).catch((err) => {
+        res.status(500).json({ err, message: 'Failed to get user' });
+
+
+    });
+
+});
+
+
+
+
+
+route.post('/delete', (req, res) => {
+
+    User.destroy({
+        where: {
+            [Op.and]: [{ email_add: req.body.email_add }, { password: req.body.password }]
+        }
+    }).then((user) => {
+
+        if (user == 0) {
+
+            res.status(401).json({ message: 'Incorrect user details' });
+
+        }
+
+        else {
+
+            res.status(200).json({ message: 'Account successfuly deleted' });
+        }
+
+
+    }).catch((err) => {
+
+        res.status(500).json(err);
+
+    });
+
+
+});
+
+exports = module.exports = route;
